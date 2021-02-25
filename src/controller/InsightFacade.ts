02a469389e5eae09908969367ed84c6f836b4294
kevinhuang <kevin.huang@alumni.ubc.ca>
2021-02-24 20:32:39 -0800

    fixing syntax --no-verify

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -3,8 +3,8 @@ import {IInsightFacade, InsightDataset, InsightDatasetKind} from "./IInsightFaca
 import { InsightError, NotFoundError } from "./IInsightFacade";
 import * as JSZip from "jszip";
 import { strict } from "assert";
+import {readdir, readFileSync, readlinkSync, writeFileSync} from 'fs-extra';
 // import * as fse from "fs-extra";
-const fse = require('fs-extra');
 
 /**
  * This is the main programmatic entry point for the project.
@@ -12,132 +12,77 @@ const fse = require('fs-extra');
  *
  */
 export default class InsightFacade implements IInsightFacade {
-    addedMap = new Map(); // global variable thing TOASK4
+    private addedMap: Map<string, string>;
+
     constructor() {
-        const fs = require('fs');
-        function reviver(key: any, value: any) {
-            if(typeof value === 'object' && value !== null) {
-              if (value.dataType === 'Map') {
-                return new Map(value.value);
-              }
-            }
-            return value;
-        }
+        // function reviver(key: any, value: any) {
+        //     if(typeof value === 'object' && value !== null) {
+        //       if (value.dataType === 'Map') {
+        //         return new Map(value.value);
+        //       }
+        //     }
+        //     return value;
+        // }
         // this.addedMap = this.loadDiskDatasets();
+        this.addedMap = new Map();
         Log.trace("InsightFacadeImpl::init()");
     }
 
-    // public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-    //     let newZip = new JSZip();
-    //     let dataSetMap = new Map();
-    //     // if (this.validId(id) === false) {
-    //     //     Promise.reject(new InsightError());
-    //     // }
-        
-    //     // https://stuk.github.io/jszip/documentation/examples.html
-    //     // https://stackoverflow.com/questions/39322964/extracting-zipped-files-using-jszip-in-javascript
-    //     // dir is true and assume id is correct (private helpers?), need to store types (TOASKNEW3)
-    //     newZip.loadAsync(content, { base64: true }).then(function (zip: any) {
-    //         let promisesArr = [Promise];
-    //         zip.folder("courses").forEach(function (relativePath: any, file: any) {
-    //             let name = file.name.replace("courses/", "");
-    //             promisesArr.push(file.async("string").then(function (content: any) {
-    //                 // parseCourseData(content).then(function (courseData: any) {
-    //                 dataSetMap.set(name, content);
-    //                 // ?? name async ? JSONPARSE
-    //                 // } use a class for getting right fields? (TOASK2)
-    //                 // ...then
-    //             })
-    //             )
-    //         });
-    //         Promise.all(promisesArr)
-    //             .catch(return Promise.reject("Not implemented."));
-    //     }).then(() => {
-    //         // let newCourses = newZip.folder("courses"); Promise.all (TOASK1)
-    //         // let testMap = new Map();
-    //         // what to do with result? (TOASK)
-    //         // testMap.set("AANB504", '{"result":[{"tier_eighty_five":1,"tier_ninety":8,"Title":"rsrch methdlgy","Section":"002","Detail":"","tier_seventy_two":0,"Other":1,"Low":89,"tier_sixty_four":0,"id":31379,"tier_sixty_eight":0,"tier_zero":0,"tier_seventy_six":0,"tier_thirty":0,"tier_fifty":0,"Professor":"","Audit":9,"tier_g_fifty":0,"tier_forty":0,"Withdrew":1,"Year":"2015","tier_twenty":0,"Stddev":2.65,"Enrolled":20,"tier_fifty_five":0,"tier_eighty":0,"tier_sixty":0,"tier_ten":0,"High":98,"Course":"504","Session":"w","Pass":9…gy", "Section": "overall", "Detail": "", "tier_seventy_two": 0, "Other": 1, "Low": 89, "tier_sixty_four": 0, "id": 31380, "tier_sixty_eight": 0, "tier_zero": 0, "tier_seventy_six": 0, "tier_thirty": 0, "tier_fifty": 0, "Professor": "", "Audit": 9, "tier_g_fifty": 0, "tier_forty": 0, "Withdrew": 1, "Year": "2015", "tier_twenty": 0, "Stddev": 2.65, "Enrolled": 20, "tier_fifty_five": 0, "tier_eighty": 0, "tier_sixty": 0, "tier_ten": 0, "High": 98, "Course": "504", "Session": "w", "Pass": 9, "Fail": 0, "Avg": 94.44, "Campus": "ubc", "Subject": "aanb"}], "rank": 0}');
-    //         function replacer(key: any, value: any) {
-    //             if (value instanceof Map) {
-    //                 return {
-    //                     dataType: 'Map',
-    //                     value: Array.from(value.entries()),
-    //                 };
-    //             } else {
-    //                 return value;
-    //             }
-    //         }
-
-    //         function reviver(key: any, value: any) {
-    //             if (typeof value === 'object' && value !== null) {
-    //                 if (value.dataType === 'Map') {
-    //                     return new Map(value.value);
-    //                 }
-    //             }
-    //             return value;
-    //         }
-
-    //         // const str = JSON.stringify(testMap, replacer);
-    //         // this.addedMap.set(id, testMap);
-    //         // fse.writeFileSync("./data/" + id + ".txt", str); //TOASK5 sync or async for file commands
-    //         // let addedIds = Array.from(this.addedMap.keys());
-    //         // let data = fse.readFileSync("./data/courses.txt");
-    //         // const newValue = JSON.parse(data, reviver);
-    //         return Promise.reject("Not implemented.");
-    //     })
-    //     return Promise.reject("Not implemented.");
     // }  https://stackoverflow.com/questions/47746760/js-how-to-solve-this-promise-thing
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
         let newZip = new JSZip();
         let dataSetMap = new Map();
+        let fileName: string;
         // this.parseCourseData("test");
         // return Promise.reject(new InsightError());
         // https://stackoverflow.com/questions/39322964/extracting-zipped-files-using-jszip-in-javascript
-        newZip.loadAsync(content, { base64: true }).then(function (zip: any) {
+        return newZip.loadAsync(content, { base64: true }).then(function (zip: any) {
             let promisesArr = [Promise];
             zip.folder("courses").forEach(function (relativePath: any, file: any) {
-                let name = file.name.replace("courses/", "");
-                promisesArr.push(file.async("string").then(function (content: any) {  //TOASKNEW1
-                    // this.parseCourseData(id, content).then(function (courseData: any) {
-                    //     dataSetMap.set(name, courseData);
-                    //     // ?? name async ? JSONPARSE
-                    //     // } use a class for getting right fields? (TOASK2)
-                    //     // ...then
-                        
-                    // })
-                    dataSetMap.set(name, content);
-                })
-                )
+                fileName = file.name.replace("courses/", "");
+                promisesArr.push(file.async("string"))
             });
-            Promise.all(promisesArr)
-                .catch() // InsightError?
-                .then(() => {
+            return Promise.all(promisesArr)              
+                .then((content: any) => {
+                    dataSetMap.set(fileName, content);
                     function replacer(key: any, value: any) {
-                            if (value instanceof Map) {
-                                return {
-                                    dataType: 'Map',
-                                    value: Array.from(value.entries()),
-                                };
-                            } else {
-                                return value;
-                            }
+                        if (value instanceof Map) {
+                            return {
+                                dataType: 'Map',
+                                value: Array.from(value.entries()),
+                            };
+                        } else {
+                            return value;
+                        }
                     }
-                    const str = JSON.stringify(dataSetMap, replacer);
-                    try {
-                        fse.writeFileSync("./data/" + id + ".txt", str);
-                    } catch (err) {
-                        Log.info("error w/ saving");
-                    }
+                    const str = JSON.stringify(dataSetMap, replacer);                  
+                    // (node:4405) UnhandledPromiseRejectionWarning: TypeError: Cannot read property 'addedMap' of undefined
+                    // at /Users/kevinhuang/Documents/project_team226/src/controller/InsightFacade.ts:131:26
+                    //(Use `node --trace-warnings ...` to show where the warning was created)
+                    // (node:4405) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by 
+                    // throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch().
+                    // To terminate the node process on unhandled promise rejection, use the CLI flag`--unhandled-rejections=strict`(see https://nodejs.org/api/cli.html#cli_unhandled_rejections_mode). (rejection id: 3)
+                    // but data still writes to disk??
+                    writeFileSync("./data/" + id + ".txt", str);        
                     this.addedMap.set(id, dataSetMap);
-                    Log.info(this.addedMap);
-                    let addedIds = Array.from(this.addedMap.keys());
-                    return Promise.resolve(addedIds);
-            });
-        }).catch(() => {
-            Log.info("not here");
-            return Promise.reject("Not implemented.");
+                    // Log.info(this.addedMap);
+                    // let addedIds: Array<string> = Array.from(this.addedMap.keys());
+                    // let addedIds: Array<string>;
+                    // addedIds.push(id);
+                    // let dupe = addedIds;
+                    Log.info("SUCCESS");
+                    return []; //diff between return the array vs Promise.resolve(arr)?
+                });
+        }).then(() => {
+            Log.info("MORE SUCCESS")
+            return ['1'];
         })
-        return Promise.reject(new InsightError()); //return Promise.resolve(addedIds);
+            .catch((err) => {
+                Log.info(err);
+                Log.info("UH oH")
+            return Promise.reject(new InsightError());
+        })
+        // return Promise.reject(new InsightError()); //return Promise.resolve(addedIds);
     }
 
     public removeDataset(id: string): Promise<string> {
@@ -209,13 +154,13 @@ export default class InsightFacade implements IInsightFacade {
             }
             return value;
           }
-        fse.readdir("./data/", function (err: any, filenames: any) {
+        readdir("./data/", function (err: any, filenames: any) {
             if (err) {
                 Log.info("Failed directory read");
                 return;
             }
             filenames.forEach(function (filename: any) {
-                let data = fse.readFileSync(filename);
+                let data = readlinkSync(filename);
                 let name = filename.name.replace("courses/", "");
                 const newMapValue = JSON.parse(data, this.reviver());
                 resMap.set(name, newMapValue);
@@ -226,6 +171,7 @@ export default class InsightFacade implements IInsightFacade {
     // https://stackoverflow.com/questions/20059995/how-to-create-an-object-from-an-array-of-key-value-pairs/43682482
     parseCourseData(id: string, content: string): string {
         // Object.fromEntries = arr => Object.assign({}, ...Array.from(arr, ([k, v]) => ({[k]: v}) ));
+        // how to check if all course keys needed are in the json, length?
         const courseKeys = [
             "Subject", // CPSC
             "Section",
@@ -244,9 +190,9 @@ export default class InsightFacade implements IInsightFacade {
         let obj = JSON.parse(dummy);
         let sectionOverall = false;
         let newYearKey = "";
-        // let newJSON = {
-        //     [key: String]: any //TOASKNEW2
-        // };
+        let newJSON = {
+            // [key: String]: any //TOASKNEW2
+        };
         Object.keys(obj.result[0]).forEach((key) => {
             if (courseKeys.indexOf(key) !== -1) {
                 Log.info(key);
@@ -260,11 +206,12 @@ export default class InsightFacade implements IInsightFacade {
                 // if (key == "Year") {
                 //     newYearKey = newKey;
                 // }
-                //let newVal = this.enforceTypes(key, obj.result[0][key])
+                // let newVal = this.enforceTypes(key, obj.result[0][key])
                 // newJSON[key] = obj.result[0][key];
+                //Element implicitly has an 'any' type because index expression is not of type 'number'.
             }
         })
-
+        // This condition will always return 'false' since the types 'false' and 'true' have no overlap.ts(2367)
         // if (sectionOverall === true) {
         //     newJSON[newYearKey] = 1900;
         // }
