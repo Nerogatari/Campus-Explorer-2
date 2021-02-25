4acc4258f434b851e66f4b08817cf4868156991a
kevinhuang <kevin.huang@alumni.ubc.ca>
2021-02-25 15:41:39 -0800

    separated helpers into another file

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -2,12 +2,14 @@ import Log from "../Util";
 import {IInsightFacade, InsightDataset, InsightDatasetKind} from "./IInsightFacade";
 import { InsightError, NotFoundError } from "./IInsightFacade";
 import * as JSZip from "jszip";
-import { strict, throws } from "assert";
+import DatasetHelper from "./DatasetHelpers";
 import {readdir, readFileSync, readlinkSync, unlinkSync, writeFileSync} from "fs-extra";
 // import * as fse from "fs-extra";
 export default class InsightFacade implements IInsightFacade {
     private addedMapsArr: any[];
+    private datasetHelper: DatasetHelper;
     constructor() {
+        this.datasetHelper = new DatasetHelper();
         this.addedMapsArr = [];
         this.loadDiskDatasets();
         Log.trace("InsightFacadeImpl::init()");
@@ -22,12 +24,12 @@ export default class InsightFacade implements IInsightFacade {
         if (kind === InsightDatasetKind.Rooms) {
             return Promise.reject(new InsightError("Invalid Kind"));
         }
-        if (this.validId(id) === false) {
+        if (this.datasetHelper.validId(id) === false) {
             return Promise.reject(new InsightError("Invalid ID"));
         }
         if (this.existingDatasetID(id) === true) {
             return Promise.reject(new InsightError("Existing ID"));
-        }
+        } // TODO check weird, illformed files, weird courses structure
         // https://stackoverflow.com/questions/39322964/extracting-zipped-files-using-jszip-in-javascript
         return newZip.loadAsync(content, { base64: true }).then((zip: any) => {
             let promisesArr: Array<Promise<string>> = [];
@@ -42,7 +44,7 @@ export default class InsightFacade implements IInsightFacade {
                 .then((sectionData: any) => {
                     let tempArr = [];
                     for (const section of sectionData) {
-                        tempArr = this.parseCourseData(id, section);
+                        tempArr = this.datasetHelper.parseCourseData(id, section);
                         dataSetArray.push(...tempArr);
                     }
                     if (dataSetArray.length === 0) {
@@ -69,7 +71,7 @@ export default class InsightFacade implements IInsightFacade {
 
     public removeDataset(id: string): Promise<string> {
         let removedId = "";
-        if (this.validId(id) === false) {
+        if (this.datasetHelper.validId(id) === false) {
             return Promise.reject(new InsightError("Invalid ID"));
         }
         if (this.existingDatasetID(id) === false) {
@@ -78,14 +80,12 @@ export default class InsightFacade implements IInsightFacade {
         for (let i = 0; i < this.addedMapsArr.length; i++) {
             if (this.addedMapsArr[i].id === id) {
                 this.addedMapsArr.splice(i, 1);
-                let bool = this.existingDatasetID(id);
                 removedId = id;
             }
         }
         unlinkSync("./data/" + id + ".txt");
         return Promise.resolve(removedId);
         // use unlink for async TODO if running time too long
-        // fse.unlinkSync("./data/" + id + ".txt");
     }
 
     public performQuery(query: any): Promise<any[]> {
@@ -102,34 +102,8 @@ export default class InsightFacade implements IInsightFacade {
         }
         return Promise.resolve(emptyList);
     }
-
-    private validId(id: string): boolean {
-        // TODO TEST regex more
-        // https://stackoverflow.com/questions/2031085/
-        // how-can-i-check-if-string-contains-characters-whitespace-not-just-whitespace/6610847
-        if ((id === null) || (id === undefined) || (!/\S/.test(id)) ||
-            (id.includes("_") || (!/^[^_]+$/.test(id)))) {
-            return false;
-            // string is not empty and not just whitespace, need to add more checks here
-        } else {
-            return true;
-        }
-    }
-
+    // should stay
     private existingDatasetID(id: string): boolean {
-        // let test = [];
-        // let obj1 = {
-        //     id: "courses",
-        //     kind: "courses",
-        //     data: ['hello']
-        // }
-        // let obj2 = {
-        //     id: "kevin",
-        //     kind: "courses",
-        //     data: ['not today']
-        // }
-        // test.push(obj1);
-        // test.push(obj2);
         let bool: boolean = false;
         bool = this.addedMapsArr.some((ele) => {
             return ele.id === id;
@@ -149,128 +123,11 @@ export default class InsightFacade implements IInsightFacade {
                 }
                 let data = JSON.parse(readFileSync("./data/" + filename).toString());
                 if (this.existingDatasetID(data.id) === true) {
-                    Log.info("Skipped loading id:" + data.id + ", due to already existing id");
+                    Log.warn("Skipped loading id:" + data.id + ", due to already existing id");
                     return;
                 }
                 this.addedMapsArr.push(data);
             });
         });
-    }
-    // https://stackoverflow.com/questions/54905976/how-do-i-filter-keys-from-json-in-node-js
-    // https://stackoverflow.com/questions/20059995/how-to-create-an-object-from-an-array-of-key-value-pairs/43682482
-    private parseCourseData(id: string, content: string): any[] {
-        // how to check if all course keys needed are in the json, length?
-        const courseKeys = [
-            "Subject", // CPSC
-            "Section",
-            "Professor",
-            "Audit",
-            "Year",
-            "Course", // 310
-            "Title",
-            "Pass",
-            "Fail",
-            "Avg",
-            "id"
-        ];
-        let jsonObj = JSON.parse(content);
-        let newYearKey = "";
-        let newJSONArr: any = [];
-        jsonObj.result.forEach((ele: any) => {
-            let newJSON: any = {};
-            let sectionOverall: boolean = false;
-            Log.info(this.validateSections(ele, courseKeys));
-            if (this.validateSections(ele, courseKeys) === false) {
-                return;
-            }
-            Object.keys(ele).forEach((key) => {
-                if (courseKeys.indexOf(key) !== -1) {
-                    if (key === "Section") {
-                        if (ele[key] === "overall") {
-                            sectionOverall = true;
-                        }
-                        return;
-                    }
-                    let newKey = this.convertKeysWithId(id, key);
-                    if (key === "Year") {
-                        newYearKey = newKey;
-                    }
-                    let newVal = this.enforceTypes(key, ele[key]);
-                    newJSON[newKey] = newVal;
-                }
-            });
-            if (sectionOverall) {
-                newJSON[newYearKey] = 1900;
-            }
-            newJSONArr.push(newJSON);
-        });
-        return newJSONArr;
-    }
-    private convertKeysWithId(id: string, key: string): string {
-        let newKey = "";
-        switch (key) {
-            case "Subject":
-                newKey = "dept";
-                break;
-            case "Course":
-                newKey = "id";
-                break;
-            case "Avg":
-                newKey = "avg";
-                break;
-            case "Professor":
-                newKey = "instructor";
-                break;
-            case "Title":
-                newKey = "title";
-                break;
-            case "Pass":
-                newKey = "pass";
-                break;
-            case "Fail":
-                newKey = "fail";
-                break;
-            case "Audit":
-                newKey = "audit";
-                break;
-            case "id":
-                newKey = "uuid";
-                break;
-            case "Year":
-                newKey = "year";
-        }
-        return id + "_" + newKey;
-    }
-    // what happens if string can't be converted to num -> insight error? TODO, TOASK
-    private enforceTypes(key: string, val: any): any {
-        // try catch and skip erroneous converts
-        let newVal: any;
-        switch (key) {
-            case "Avg":
-            case "Pass":
-            case "Fail":
-            case "Audit":
-            case "Year":
-                newVal = Number(val);
-                break;
-            case "Subject":
-            case "id":
-            case "Professor":
-            case "Course":
-            case "Title":
-                newVal = String(val);
-        }
-        return newVal;
-    }
-
-    private validateSections(data: any, keys: string[]): boolean {
-        // has all needed categories TODO TEST
-        let bool: boolean = true;
-        for (const key of keys) {
-            if (!data.hasOwnProperty(key)) {
-                return bool = false;
-            }
-        }
-        return bool;
     }
 }
